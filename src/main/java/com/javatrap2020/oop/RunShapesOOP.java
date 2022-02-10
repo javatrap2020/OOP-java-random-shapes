6d20e0b793c9d4d83d0cd69e845b4904057da77d
Oleh Boiko <javatrap2020@gmail.com>
2022-02-10 12:08:55 +0200

    add to RunShapeaOOP - List ShapeCreator ...etc

--- src/main/java/com/javatrap2020/oop/RunShapesOOP.java
+++ src/main/java/com/javatrap2020/oop/RunShapesOOP.java
@@ -1,12 +1,35 @@
 package com.javatrap2020.oop;
 
+import com.javatrap2020.oop.shapes.ShapeCreator;
+import com.javatrap2020.oop.shapes.ShapeInterface;
+import com.javatrap2020.oop.shapes.circle.CircleCreator;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+
+import static com.javatrap2020.oop.shapes.ClassGenerator.GenerateValue;
 
 public class RunShapesOOP {
 
     public static Random random = new Random();
 
     public static void main(String[] args) {
+
+        List<ShapeCreator> randomShapes = new ArrayList<>();
+        randomShapes.add(new CircleCreator());
+
+        int SIZE = (int)  GenerateValue(1, 10);
+
+        ShapeInterface[] shapeBases = new ShapeInterface[SIZE];
+
+        for (int i = 0; i < SIZE; i++) {
+            shapeBases[i] = randomShapes.get((int)GenerateValue(0, randomShapes.size())).Doing();
+        }
+
+        for (ShapeInterface i : shapeBases) {
+            i.draw();
+        }
 
     }
 }
